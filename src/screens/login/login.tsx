91502d4c22e06cc1bdbc9cf2ccb51fe629f8f533
DanielGeek <daniel30081990@gmail.com>
2021-10-22 23:50:25 -0300

    59. Installing AWS Amplify Client Libraries & Adding the Login Screen

---
+++ src/screens/login/login.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import { View, Text } from 'react-native';
+
+export default function login(): ReactElement {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
