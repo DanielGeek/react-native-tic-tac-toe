ac840c6428afe72a8699d1c0603e8bfe2af6e9b5
DanielGeek <daniel30081990@gmail.com>
2021-11-15 23:56:15 -0300

    fixed 69. Account Confirmation UI & Functionality

--- src/screens/signup/signup.styles.tsx
+++ src/screens/signup/signup.styles.tsx
@@ -1,29 +1,41 @@
+import { colors } from "@utils";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  contianer: {
+  container: {
     paddingHorizontal: 30,
     paddingVertical: 40
   },
+  otpInputBox: {
+    color: colors.lightGreen,
+    fontFamily: "DeliusUnicase_400Regular",
+    fontSize: 20,
+    borderWidth: 0,
+    borderRadius: 0,
+    backgroundColor: colors.purple,
+    borderBottomWidth: 1,
+    borderColor: colors.lightGreen,
+  },
+  otpActiveInputBox: {
+    borderWidth: 1,
+    borderColor: colors.lightPurple
+  },
   borderStyleBase: {
     width: 30,
-    height: 45,
-    borderColor: "#03DAC6",
-    borderWidth: 1,
-    borderBottomWidth: 1,
-    flexDirection: "column",
-    flexWrap: "wrap",
-    marginTop: 10
+    height: 45
   },
+
   borderStyleHighLighted: {
     borderColor: "#03DAC6",
   },
+
   underlineStyleBase: {
     width: 30,
     height: 45,
     borderWidth: 0,
     borderBottomWidth: 1,
   },
+
   underlineStyleHighLighted: {
     borderColor: "#03DAC6",
   },
--- src/screens/signup/signup.tsx
+++ src/screens/signup/signup.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement, useRef, useState } from 'react';
-import { View, Alert, ScrollView, TextInput as NativeTextInput, KeyboardAvoidingView, Platform } from 'react-native';
-import { GradientBackground, TextInput, Button } from '@components';
+import { Alert, ScrollView, TextInput as NativeTextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { GradientBackground, TextInput, Button, Text } from '@components';
 import { StackNavigationProp, useHeaderHeight } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
 import { Auth } from "aws-amplify";
 import OtpInputs from 'react-native-otp-inputs';
+// import OTPInput from '@twotalltotems/react-native-otp-input'
 import styles from "./signup.styles";
 
 
@@ -24,6 +25,7 @@ export default function SignUp({navigation}: SignUpProps): ReactElement {
     password: "12345678"
   });
   const [loading, setLoading] = useState(false);
+  const [step, setStep] = useState<"signUp" | "otp">("otp");
 
   const setFormInput = (key: keyof typeof form, value: string) => {
     setForm({...form, [key]: value});
@@ -44,75 +46,114 @@ export default function SignUp({navigation}: SignUpProps): ReactElement {
         console.log(res);
     } catch (error) {
         Alert.alert("Error!", error.message || "An error has occurred!");
+      }
+      setLoading(false);
+    };
+
+    const confirmCode = async (code: string) => {
+      try {
+        await Auth.confirmSignUp(form.username, code);
+        navigation.navigate("Login");
+        Alert.alert("Success!", "You can now login with your account!");
+      } catch (error) {
+        Alert.alert("Error!", error.message || "An error has occurred!");
     }
-    setLoading(false);
   }
   return (
     <GradientBackground>
       <KeyboardAvoidingView keyboardVerticalOffset={headerHeight} behavior={ Platform.OS === "ios" ? "padding": "height" } style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={styles.contianer}>
-          <TextInput
-              value={form.username}
-              onChangeText={(value) => {
-                setFormInput("username", value)
-              }}
-              returnKeyType="next"
-              placeholder="Username"
-              style={{
-                marginBottom: 20
-              }}
-              onSubmitEditing={() => {
-                nameRef.current?.focus();
-              }}
-          />
-          <OtpInputs
-            style={styles.borderStyleBase}
-            handleChange={(code) => console.log(code)}
-            numberOfInputs={6}
-          />
-          <TextInput
-              ref={nameRef}
-              value={form.name}
-              onChangeText={(value) => {
-                setFormInput("name", value)
-              }}
-              returnKeyType="next"
-              placeholder="Name"
-              style={{
-                marginBottom: 20
-              }}
-              onSubmitEditing={() => {
-                emailRef.current?.focus();
-              }}
-          />
-          <TextInput
-              keyboardType="email-address"
-              ref={emailRef}
-              value={form.email}
-              onChangeText={(value) => {
-                setFormInput("email", value)
-              }}
-              returnKeyType="next"
-              placeholder="Email"
-              style={{
-                marginBottom: 20
-              }}
-              onSubmitEditing={() => {
-                passwordRef.current?.focus();
-              }}
-          />
-          <TextInput
-              value={form.password}
-              onChangeText={(value) => {
-                setFormInput("password", value)
-              }}
-              style={{ marginBottom: 30 }}
-              ref={passwordRef}
-              returnKeyType="done"
-              secureTextEntry
-              placeholder="Password"
-          />
-          <Button loading={loading} title="Sign-Up" onPress={signUp} />
+        <ScrollView contentContainerStyle={styles.container}>
+          {
+            step === "otp"
+            &&
+              // <OTPInput
+              //   placeholderCharacter="0"
+              //   placeholderTextColor="#5d5379"
+              //   pinCount={6}
+              //   codeInputFieldStyle={styles.underlineStyleBase}
+              //   autoFocusOnLoad
+              //   codeInputHighlightStyle={styles.underlineStyleHighLighted}
+              //   // codeInputHighlightStyle={styles.otpActiveInputBox}
+              //   onCodeFilled = {(code => {
+              //       console.log(`Code is ${code}, you are good to go!`)
+              //   })}
+              // />
+              <>
+                <Text>Enter the code that you received via email.</Text>
+                <OtpInputs
+                  placeholderTextColor="#5d5379"
+                  onChange = {code => {
+                    confirmCode(code);
+                  }}
+                  style={styles.otpInputBox}
+                  numberOfInputs={6}
+                />
+              </>
+          }
+          {
+            step === "signUp" &&
+            (
+              <>
+                <TextInput
+                    value={form.username}
+                    onChangeText={(value) => {
+                      setFormInput("username", value)
+                    }}
+                    returnKeyType="next"
+                    placeholder="Username"
+                    style={{
+                      marginBottom: 20
+                    }}
+                    onSubmitEditing={() => {
+                      nameRef.current?.focus();
+                    }}
+                />
+                <TextInput
+                    ref={nameRef}
+                    value={form.name}
+                    onChangeText={(value) => {
+                      setFormInput("name", value)
+                    }}
+                    returnKeyType="next"
+                    placeholder="Name"
+                    style={{
+                      marginBottom: 20
+                    }}
+                    onSubmitEditing={() => {
+                      emailRef.current?.focus();
+                    }}
+                />
+                <TextInput
+                    keyboardType="email-address"
+                    ref={emailRef}
+                    value={form.email}
+                    onChangeText={(value) => {
+                      setFormInput("email", value)
+                    }}
+                    returnKeyType="next"
+                    placeholder="Email"
+                    style={{
+                      marginBottom: 20
+                    }}
+                    onSubmitEditing={() => {
+                      passwordRef.current?.focus();
+                    }}
+                />
+                <TextInput
+                    value={form.password}
+                    onChangeText={(value) => {
+                      setFormInput("password", value)
+                    }}
+                    style={{ marginBottom: 30 }}
+                    ref={passwordRef}
+                    returnKeyType="done"
+                    secureTextEntry
+                    placeholder="Password"
+                />
+                <Button loading={loading} title="Sign-Up" onPress={signUp} />
+              </>
+            )
+          }
         </ScrollView>
       </KeyboardAvoidingView>
     </GradientBackground>
